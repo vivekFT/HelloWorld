2f89dd1e14e05643897b77a4d80c2a8fbba46c8a
vivekFT <vivek@mofirst.com>
2016-01-06 19:00:42 +0530

    Added comment for git commit feature

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package com.github.vivek;
 
 class HelloWorld{
 	public static int printCount = 0;
-	
+	//This function is added to test the git commit feature	
 	public void printOneToTen(){
 		for(int i = 0; i < 10 ; i++){
 			System.out.println("Printing this for 10 times");
